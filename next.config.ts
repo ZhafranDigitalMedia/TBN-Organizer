744e955de9e40b1f2cceeafecccb888d6f1af6b9
Zhafran <80811273+ZhafranDigitalMedia@users.noreply.github.com>
2026-09-03 17:26:02 +0700

    update tambahan

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co', // Mendaftarkan domain Supabase Storage
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com', // Mendaftarkan Unsplash (jika pakai dummy data)
+      },
+    ],
+  },
 };
 
 export default nextConfig;
